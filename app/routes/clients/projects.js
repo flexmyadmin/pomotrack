78c4608400b1447587033be66436a6dce6b9e131
Tavi <dragulceo@gmail.com>
2014-12-07 22:55:25 +0200

    Missing semicolon and extra comma in routes files (due to wrong copy/paste)

--- app/routes/clients/projects.js
+++ app/routes/clients/projects.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function (params) {
-    return this.get('store').find('client', params.client_id)
-  },
+    return this.get('store').find('client', params.client_id);
+  }
 });
--- app/routes/clients/projects/tasks.js
+++ app/routes/clients/projects/tasks.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function (params) {
     return this.get('store').find('project', params.project_id);
-  },
+  }
 });
