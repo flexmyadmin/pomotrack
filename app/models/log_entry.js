c3aaaa842f86bf9545a2492c50fdfdcc0afde059
Gunther Konig <gunther.s.konig@gmail.com>
2014-12-09 17:28:29 +0200

    status consts are status

--- app/models/log_entry.js
+++ app/models/log_entry.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 var CONST = {
-  STARTED: 'started',
-  STOPPED: 'stopped'
+  STATUS_STARTED: 'started',
+  STATUS_STOPPED: 'stopped'
 };
 
 var LogEntry = DS.Model.extend({
@@ -15,7 +15,7 @@ var LogEntry = DS.Model.extend({
     defaultValue: 0
   }),
   status: DS.attr('string', {
-    defaultValue: CONST.STARTED
+    defaultValue: CONST.STATUS_STARTED
   }),
   task: DS.belongsTo('task'),
   stop: function() {
@@ -26,10 +26,10 @@ var LogEntry = DS.Model.extend({
     this.set('duration', new Date() - this.get('added'));
   },
   isStarted: function() {
-    return this.get('status') === CONST.STARTED;
+    return this.get('status') === CONST.STATUS_STARTED;
   }.property('status'),
   isStopped: function() {
-    return this.get('status') === CONST.STOPPED;
+    return this.get('status') === CONST.STATUS_STOPPED;
   }.property('status')
 });
 
@@ -37,52 +37,52 @@ LogEntry.reopenClass({
   FIXTURES: [{
     id: 1,
     added: new Date(2014, 11, 8, 10, 20),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 1
   }, {
     id: 2,
     added: new Date(),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 1
   }, {
     id: 3,
     added: new Date(),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 1
   }, {
     id: 4,
     added: new Date(),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 2
   }, {
     id: 5,
     added: new Date(),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 3
   }, {
     id: 6,
     added: new Date(),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 4
   }, {
     id: 7,
     added: new Date(),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 5
   }, {
     id: 8,
     added: new Date(),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 6
   }, {
     id: 9,
     added: new Date(),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 7
   }, {
     id: 10,
     added: new Date(),
-    status: CONST.STOPPED,
+    status: CONST.STATUS_STOPPED,
     task: 8
   }]
 });
