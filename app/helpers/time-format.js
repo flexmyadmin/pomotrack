d620997ca1287df718376e47f1727b9ba7d34fa9
Tavi <dragulceo@gmail.com>
2014-12-09 04:32:43 +0200

    Time formatter with miliseconds

---
+++ app/helpers/time-format.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+
+var SECONDS_PER_HOUR = 3600,
+  SECONDS_PER_MINUTE = 60;
+
+function formatInteger(value, leadingZeros) {
+  var str;
+  value = parseInt(value, 10);
+  str = value;
+  if (leadingZeros > 0) {
+    if (value < Math.pow(10, leadingZeros)) {
+      value = '' + value;
+      str = new Array(leadingZeros + 1 - value.length);
+      str = str.join('0') + value;
+    }
+  }
+  return str;
+}
+
+function format(value, hasMiliseconds) {
+  var seconds = 0,
+    minutes,
+    hours,
+    miliSeconds;
+  value = parseInt(value, 10);
+  if (isNaN(value)) {
+    value = 0;
+  }
+  if (hasMiliseconds) {
+    seconds = value / 1000;
+    miliSeconds = value % 1000;
+  } else {
+    seconds = value;
+    miliSeconds = false;
+  }
+  hours = Math.floor(seconds / SECONDS_PER_HOUR);
+  seconds = seconds - SECONDS_PER_HOUR * hours;
+  minutes = Math.floor(seconds / SECONDS_PER_MINUTE);
+  seconds = seconds - minutes * SECONDS_PER_MINUTE;
+  return formatInteger(hours, 2) + ':' +
+  formatInteger(minutes, 2) + ':' +
+  formatInteger(seconds, 2) +
+  (miliSeconds === false ? "" : "." + formatInteger(miliSeconds, 3));
+}
+
+export function timeFormat(input) {
+  return format(input);
+}
+
+export default Ember.Handlebars.makeBoundHelper(timeFormat);
