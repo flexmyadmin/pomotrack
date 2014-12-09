91af546d5c2e357529a6031e2291b05a107a6f52
Gunther Konig <gunther.s.konig@gmail.com>
2014-12-09 11:16:59 +0200

    add logEntry.status - safer than checking against "duration == 0" (for really-really fast logEntries :) )

--- app/models/log_entry.js
+++ app/models/log_entry.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
+var CONST = {
+  STARTED: 'started',
+  STOPPED: 'stopped'
+}
+
 var LogEntry = DS.Model.extend({
   added: DS.attr('date', {
     defaultValue: function () {
@@ -8,6 +13,9 @@ var LogEntry = DS.Model.extend({
   }),
   duration: DS.attr('number', {
     defaultValue: 0
+  }),
+  status: DS.attr('string', {
+    defaultValue: CONST.STARTED
   }),
   task: DS.belongsTo('task'),
   stop: function() {
@@ -23,42 +31,52 @@ LogEntry.reopenClass({
   FIXTURES: [{
     id: 1,
     added: new Date(2014, 11, 8, 10, 20),
+    status: CONST.STOPPED,
     task: 1
   }, {
     id: 2,
     added: new Date(),
+    status: CONST.STOPPED,
     task: 1
   }, {
     id: 3,
     added: new Date(),
+    status: CONST.STOPPED,
     task: 1
   }, {
     id: 4,
     added: new Date(),
+    status: CONST.STOPPED,
     task: 2
   }, {
     id: 5,
     added: new Date(),
+    status: CONST.STOPPED,
     task: 3
   }, {
     id: 6,
     added: new Date(),
+    status: CONST.STOPPED,
     task: 4
   }, {
     id: 7,
     added: new Date(),
+    status: CONST.STOPPED,
     task: 5
   }, {
     id: 8,
     added: new Date(),
+    status: CONST.STOPPED,
     task: 6
   }, {
     id: 9,
     added: new Date(),
+    status: CONST.STOPPED,
     task: 7
   }, {
     id: 10,
     added: new Date(),
+    status: CONST.STOPPED,
     task: 8
   }]
 });
