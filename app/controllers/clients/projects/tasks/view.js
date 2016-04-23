b97acc693d92fcc67a4c354d23ec09194c77ab65
dragulceo <dragulceo@gmail.com>
2016-04-23 05:22:41 +0300

    Add missing semicolon

--- app/controllers/clients/projects/tasks/view.js
+++ app/controllers/clients/projects/tasks/view.js
@@ -1,5 +1,5 @@
 import Ember from "ember";
-import CONST from '../../../../constants/log_entry'
+import CONST from '../../../../constants/log_entry';
 
 export default Ember.Controller.extend({
   actions: {
