7729a3a69a602bdf5672bb2fc0d2a49fe35a0c28
dragulceo <dragulceo@gmail.com>
2016-05-01 02:10:27 +0300

    Add temporaray selected module

--- app/routes/clients.js
+++ app/routes/clients.js
@@ -1,12 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function () {
+  model: function() {
     return this.store.findAll('client');
   },
   renderTemplate: function() {
     this.render('clients');
-
+    this.render('selected', {
+      outlet: 'selected',
+    });
     this.render('clock', {
       outlet: 'clock',
       controller: 'clock'
